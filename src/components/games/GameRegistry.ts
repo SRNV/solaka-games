a52230372c54a6bef3ec8ffa1e241dc58a2d47ef
SRNV <alula.rudy@gmail.com>
2026-05-18 21:53:35 +0200

    update

---
+++ src/components/games/GameRegistry.ts
@@ -0,0 +1,10 @@
+import { lazy, LazyExoticComponent, ComponentType } from 'react';
+
+// Type pour nos composants de jeux
+export type GameComponent = LazyExoticComponent<ComponentType<any>>;
+
+// Le Registry mappe les slugs du serveur aux imports dynamiques
+// Note: Les chemins sont relatifs à l'emplacement de ce fichier dans le submodule
+export const GAME_REGISTRY: Record<string, GameComponent> = {
+  'metel-game': lazy(() => import('../../../Metel')),
+};
